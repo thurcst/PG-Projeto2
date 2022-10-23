1938e54be2b6709bb92d67e33024a113b6d44be8
root <root@Dell_G15.localdomain>
2022-10-22 21:03:38 -0300

    Adicionado parte da fase 1

---
+++ RayTracing/cena.py
@@ -0,0 +1,10 @@
+class Cena:
+    
+    def __init__(self, camara, objetos, largura, altura):
+        self.camara = camara
+        self.objetos = objetos
+        self.largura = largura
+        self.altura = altura 
+
+
+
